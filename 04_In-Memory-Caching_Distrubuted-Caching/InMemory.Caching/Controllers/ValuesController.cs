ae2cea168c3d95e02f2a67c0740724ac874f623c
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-03-30 09:09:47 +0300

    04, In Memory and Distributed Caching

---
+++ 04_In-Memory-Caching_Distrubuted-Caching/InMemory.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Caching.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IMemoryCache memoryCache; // DI for IMemoryCache
+
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            this.memoryCache = memoryCache;
+        }
+
+        
+        [HttpGet("{name}")]
+        public IActionResult SetName([FromRoute] string name)
+        {
+            memoryCache.Set("name", name); // Set the value in cache
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetName()
+        {
+            bool IsExists = memoryCache.TryGetValue("name", out string value); // Try to get the value from cache
+
+            if (!IsExists)
+                return NotFound();
+            return Ok(value); // Get the value from cache
+        }
+
+        [HttpGet]
+        public IActionResult SetDate()
+        {
+            memoryCache.Set<DateTime>("date", DateTime.Now, options: new()
+            {
+                 AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                 SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetDate()
+        {
+            return Ok(memoryCache.Get<DateTime>("date"));
+
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -5,3 +5,15 @@
 
 <h4>Sliding Time:</h4>
 <p>Sliding Time indicates the retention of cached data in memory for a specified period. Access to the cache within the specified time period will extend the lifespan of the data. Otherwise, if there is no access within the specified timeframe, the cache will be cleared.</p>
+
+<h4>Distributed Caching İşlem Sırası</h4>
+
+<ul>
+    <li>StackExchangeRedis kütüphanesi yüklenir.</li>
+    <li>AddStackExchangeRedisCache servisi uygulama eklenir.</li>
+    <li>IDistributedCache referansı inject edilir.</li>
+    <li>SetString metodu ile metinsel, Set metodu ile de binary olarak veriler Redis server'a cachlenebilir. (GetString & Get)</li>
+    <li>Remove metodu ile cachelenmiş veri silinebilir.</li>
+</ul>
+
+
